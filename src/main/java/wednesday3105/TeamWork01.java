0e0a3c621844e4527fec1040a56dfd580823938f
Benna Sozen <benna.sozen@gmail.com>
2023-05-31 01:15:49 +0200

    v1

--- src/main/java/wednesday3105/TeamWork01.java
+++ src/main/java/wednesday3105/TeamWork01.java
@@ -1,5 +1,7 @@
 package wednesday3105;
 
+import java.util.Scanner;
+
 public class TeamWork01 {
     public static void main(String[] args) {
         /* Task1
@@ -7,19 +9,52 @@ public class TeamWork01 {
             Atıldıktan sonra, atıldığı yüksekliğin 3/4 u kadar yerden yukarı doğru zıplamaktadır
             Top zıplama  yüksekliği 1 metrenin altına indiğinde durmaktadır.
             Bu ana kadar aldigi toplam yolu ve yere vurma sayısını bulan do while code blogu create ediniz.*/
-       
 
+            double atilanYukseklik = 10; //Baslangic metersi
+            double sekenTop    = atilanYukseklik*3/4;//sektiginde ciktigi yukseklik
+            double toplamyol   = atilanYukseklik;
+            int vurmaSayisi = 0;
+
+            do {
+                atilanYukseklik = sekenTop;
+                toplamyol += atilanYukseklik;
+                vurmaSayisi++;
+                sekenTop = atilanYukseklik*3/4;     //yeni ziplama yuksekligi
+
+               }while (sekenTop >=1);
+
+            System.out.println("Toplam yol: "+ toplamyol+" metre");
+            System.out.println("Top "+vurmaSayisi+" kere yere vurdu.");
 
         /* Task2 :
             Verilen bir sayının faktöriyelini özyineli (recursive) olarak
             hespalayan fonksiyonu yazınız.
+                        Örnek Ekran Çıktıları
+                        Bir sayi giriniz: 6*5*4*3*2*1=720
+                        Faktöriyeli: 720
+                        Bir sayı giriniz: 3*2*1
+                        Faktöriyeli 6                       */
 
-            Örnek Ekran Çıktıları
-            Bir sayi giriniz: 6*5*4*3*2*1=720
-            Faktöriyeli: 720
+            Scanner input = new Scanner(System.in);
+            System.out.println("Lutfen bir sayi giriniz..");
+            int sayi = input.nextInt();
 
-            Bir sayı giriniz: 3*2*1
-            Faktöriyeli 6                       */
+            int faktoriyel =1;
+            String faktoriyeFormul = "";
+
+            for (int i = sayi;  i>=1;   i--){
+                faktoriyel *=i;
+                faktoriyeFormul += i;
+                if (i !=1){
+                    faktoriyeFormul += "*";
+                }
+            }
+            System.out.println("Faktoriyel Formulu: " +faktoriyeFormul);
+            System.out.println("Faktoriyeli: "+ faktoriyel);
+
+
+
+
 
 
      /*  task3:
