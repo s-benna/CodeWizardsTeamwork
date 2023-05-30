25fb050adde0ca04a2193991b3b5daccb3ed3541
Benna Sozen <benna.sozen@gmail.com>
2023-05-31 00:05:50 +0200

    Initial commit

---
+++ src/main/java/wednesday3105/TeamWork01.java
@@ -0,0 +1,47 @@
+package wednesday3105;
+
+public class TeamWork01 {
+    public static void main(String[] args) {
+        /* Task1
+            Bir top belirli yükseklikten atılmaktadır.
+            Atıldıktan sonra, atıldığı yüksekliğin 3/4 u kadar yerden yukarı doğru zıplamaktadır
+            Top zıplama  yüksekliği 1 metrenin altına indiğinde durmaktadır.
+            Bu ana kadar aldigi toplam yolu ve yere vurma sayısını bulan do while code blogu create ediniz.*/
+       
+
+
+        /* Task2 :
+            Verilen bir sayının faktöriyelini özyineli (recursive) olarak
+            hespalayan fonksiyonu yazınız.
+
+            Örnek Ekran Çıktıları
+            Bir sayi giriniz: 6*5*4*3*2*1=720
+            Faktöriyeli: 720
+
+            Bir sayı giriniz: 3*2*1
+            Faktöriyeli 6                       */
+
+
+     /*  task3:
+        Girilen bir sayı kadar satır ve sütünu olan ve
+        sağ kenara dayalı üçgeni basan kodu yazınız.
+
+        Ekran Çıktısı
+        Bir sayi giriniz: 5
+            *
+           **
+          ***
+         ****
+        *****
+
+        */
+
+    /* task4 :
+	 kullanicin girdigi sayi kadar int array olusturan ve array elemanlarini kullanicidan alan
+	 ve girilen array elemanlarinin en buyuk ile en kucuk elemaninin arasinki farki gosteren
+	 java code create ediniz.
+
+	 */
+
+    }
+}
